8a1b99e3a805321576eb3a680cd436b18f045a8c
Manuel Valbuena <valbuenamoralesmanuelalejandro@gmail.com>
2023-11-10 08:45:41 -0500

    Instalación de Express.js y mi primer servidor HTTP

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi primer server en express');
+});
+
+app.listen(port, () => {
+  console.log('Mi port es: ' + port);
+});
