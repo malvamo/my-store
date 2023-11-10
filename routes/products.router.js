f517e202d753f74770516a0445b2443663893d65
Manuel Valbuena <valbuenamoralesmanuelalejandro@gmail.com>
2023-11-10 10:29:28 -0500

    PUT, PATCH y DELETE

--- routes/products.router.js
+++ routes/products.router.js
@@ -38,4 +38,22 @@ router.post('/', (req, res) => {
   });
 });
 
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
+
 module.exports = router
