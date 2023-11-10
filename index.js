0b74c30544660fcc2a0335821c960e307c0fa300
Manuel Valbuena <valbuenamoralesmanuelalejandro@gmail.com>
2023-11-10 08:50:33 -0500

    Routing con Express.js

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
   res.send('Hola mi primer server en express');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  });
+});
+
 app.listen(port, () => {
   console.log('Mi port es: ' + port);
 });
