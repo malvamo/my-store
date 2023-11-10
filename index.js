41eae65059e96d92ba8301d9ba938d887f13bfac
Manuel Valbuena <valbuenamoralesmanuelalejandro@gmail.com>
2023-11-10 12:22:46 -0500

    Middleware para HttpErrors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const { logErrors, errorHandler } = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3000;
 
@@ -14,8 +16,12 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
+routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
+
 app.listen(port, () => {
   console.log('Mi port es: ' + port);
 });
 
-routerApi(app);
